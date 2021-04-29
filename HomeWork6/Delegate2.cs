00acd850c614f9b2d2bc4c7f18f06636a1948412
NereyFonCat <82173066+NereyFonCat@users.noreply.github.com>
2021-04-30 03:01:13 +0900

    asdasdasff

--- HomeWork6/Delegate2.cs
+++ HomeWork6/Delegate2.cs
@@ -1,25 +1,25 @@
 ﻿
-using System;
+//using System;
 
-namespace HomeWork6
-{
-    internal class Delegate2
-    {
-        // Создаем метод для передачи его в качестве параметра в Table
-        public static double MyFunc(double x, double a)
-        {
-            return x * x * a;
-        }
-        // Создаем метод, который принимает делегат
-        public static void Table(Fun F, double x, double b, double a)
-        {
-            Console.WriteLine("----- X ----- Y -----");
-            while (x <= b)
-            {
-                Console.WriteLine("| {0,8:0.000} | {1,8:0.000} |", x, F(x, a));
-                x += 1;
-            }
-            Console.WriteLine("---------------------");
-        }
-    } 
-}
+//namespace HomeWork6
+//{
+//    internal class Delegate2
+//    {
+//        // Создаем метод для передачи его в качестве параметра в Table
+//        public static double MyFunc(double x, double a)
+//        {
+//            return x * x * a;
+//        }
+//        // Создаем метод, который принимает делегат
+//        public static void Table(Fun F, double x, double b, double a)
+//        {
+//            Console.WriteLine("----- X ----- Y -----");
+//            while (x <= b)
+//            {
+//                Console.WriteLine("| {0,8:0.000} | {1,8:0.000} |", x, F(x, a));
+//                x += 1;
+//            }
+//            Console.WriteLine("---------------------");
+//        }
+//    } 
+//}
